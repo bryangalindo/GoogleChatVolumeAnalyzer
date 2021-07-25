4191433a417a713d3ebf11d52d172678d98195d0
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-25 14:13:15 -0500

    Updating variable name to more descriptive name

--- constants.py
+++ constants.py
@@ -10,6 +10,5 @@ READ_WRITE_SCOPE = 'https://www.googleapis.com/auth/spreadsheets'
 TOKEN_JSON_FILE = 'token.json'
 CREDENTIALS_JSON_FILE = 'credentials.json'
 GOOGLE_PRODUCT = 'sheets'
-VERSION = 'v4'
+PRODUCT_VERSION = 'v4'
 VALUE_INPUT_OPTION = 'RAW'
-
