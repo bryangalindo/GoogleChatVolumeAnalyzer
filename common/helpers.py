04f05ae9ff18941350cb7528b1a3eb5c219c46ae
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-26 18:07:14 -0500

    Adding unit tests for helper functions

--- common/helpers.py
+++ common/helpers.py
@@ -1,10 +1,14 @@
 from itertools import chain
 
 
-def create_values_dict(values):
+def create_values_dict(values: list) -> dict:
     return {
         'values': values,
     }
 
-def flatten_list(_list):
-    return list(chain.from_iterable(_list))
+def flatten_list(_list: list) -> list:
+    if _list is not None and _list != '':
+        if type(_list) == int:
+            raise TypeError(1)
+        else:
+            return list(chain.from_iterable(_list))
---
+++ tests/test_helpers.py
@@ -0,0 +1,32 @@
+import pytest 
+
+from common import helpers
+
+
+class TestCreateValuesDictFunction:
+    def test_valid_values(self):
+        _dict = helpers.create_values_dict("Test")
+        assert _dict == {'values': "Test"}
+        
+    def test_null_value(self):
+        _dict = helpers.create_values_dict(None)
+        assert _dict == {'values': None}
+        
+    def test_blank_value(self):
+        _dict = helpers.create_values_dict('')
+        assert _dict == {'values': ''}
+
+class TestFlattenListFunction:
+    def test_valid_list(self):
+        _list = [["Test 1"], ["Test 2"]]
+        assert helpers.flatten_list(_list) == ["Test 1", "Test 2"]
+    
+    def test_null_value(self):
+        assert helpers.flatten_list(None) == None
+        
+    def test_empty_string(self):
+        assert helpers.flatten_list('') == None
+        
+    def test_int_value(self):
+        with pytest.raises(TypeError):
+            helpers.flatten_list(6)
