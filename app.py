0d3d17c12d3a46b68694f5a742decff0e4dec0c9
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-26 11:54:28 -0500

    Adding blank line after code for PEP8 standard

--- app.py
+++ app.py
@@ -51,10 +51,4 @@ def on_event():
     
 
 if __name__ == '__main__':
-    # scopes = [c.READ_WRITE_SCOPE]
-    # oauth_credentials = GoogleCredentials(c.TOKEN_JSON_FILE, c.CREDENTIALS_JSON_FILE, scopes).get_oauth_credentials()
-    # service = GoogleService(oauth_credentials, c.GOOGLE_PRODUCT, c.PRODUCT_VERSION)
-    # values = ['Test', 'Test 2', 'Test 10',]
-    # body = h.create_values_dict(values)
-    # service.insert_row_into_spreadsheet(body, c.SPREADSHEET_ID, c.SHEET_RANGE, c.VALUE_INPUT_OPTION)
     app.run(port=8080, debug=True)
