3b75208afa950d1255220bdb17cb0249a44ea0b2
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-26 18:43:57 -0500

    Removing string literals from class methods

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,35 +1,40 @@
 import pytest 
-
 from common import helpers
 
 
+_STRING = "Test"
+_NULL = None
+_INT = 6
+EMPTY_LIST = []
+EMPTY_STRING = ''
+NESTED_LIST = [["Test 1"], ["Test 2"]]
+
 class TestCreateValuesDictFunction:
     def test_valid_values(self):
-        _dict = helpers.create_values_dict("Test")
-        assert _dict == {'values': "Test"}
+        _dict = helpers.create_values_dict(_STRING)
+        assert _dict == {'values': _STRING}
         
     def test_null_value(self):
-        _dict = helpers.create_values_dict(None)
-        assert _dict == {'values': None}
+        _dict = helpers.create_values_dict(_NULL)
+        assert _dict == {'values': _NULL}
         
     def test_blank_value(self):
-        _dict = helpers.create_values_dict('')
-        assert _dict == {'values': ''}
+        _dict = helpers.create_values_dict(EMPTY_STRING)
+        assert _dict == {'values': EMPTY_STRING}
 
 class TestFlattenListFunction:
     def test_valid_list(self):
-        _list = [["Test 1"], ["Test 2"]]
-        assert helpers.flatten_list(_list) == ["Test 1", "Test 2"]
+        assert helpers.flatten_list(NESTED_LIST) == ["Test 1", "Test 2"]
     
     def test_null_value(self):
-        assert helpers.flatten_list(None) == None
+        assert helpers.flatten_list(_NULL) == _NULL
         
     def test_empty_string(self):
-        assert helpers.flatten_list('') == None
+        assert helpers.flatten_list(EMPTY_STRING) == _NULL
         
     def test_int_value(self):
         with pytest.raises(TypeError):
-            helpers.flatten_list(6)
+            helpers.flatten_list(_INT)
     
     def test_empty_list(self):
-        assert helpers.flatten_list([]) == None
+        assert helpers.flatten_list(EMPTY_LIST) == _NULL
