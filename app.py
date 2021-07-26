987ec9b17e402db11536eb3a822c0ed9b611527e
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-26 11:53:28 -0500

    Filtering data from request json

--- app.py
+++ app.py
@@ -9,6 +9,17 @@ from models.GoogleService import GoogleService
 
 app = Flask(__name__)
 
+def create_filtered_dict(_dict):
+    room_path_list = _dict['message']['thread']['name'].split('/')
+    return {
+        'timestamp': _dict['eventTime'],
+        'email': _dict['message']['sender']['email'],
+        'room_id': room_path_list[1],
+        'room_thread': room_path_list[3],
+        'room_name': _dict['message']['space']['displayName'],
+        'message': _dict['message']['argumentText'],
+    }
+
 def update_google_spreadsheet(record):
     scopes = [constants.READ_WRITE_SCOPE]
     creds = GoogleCredentials(constants.TOKEN_JSON_FILE, constants.CREDENTIALS_JSON_FILE, scopes)
@@ -25,10 +36,18 @@ def update_google_spreadsheet(record):
 @app.route('/', methods=['POST'])
 def on_event():
     """Handles an event from Google Chat."""
-    event = request.get_json()
-    text = str(event)
-    update_google_spreadsheet(text)
-    return json.jsonify({'text': text})
+    event_dict = request.get_json()
+    filtered_event_dict = create_filtered_dict(event_dict)
+    values = [
+        filtered_event_dict['email'],
+        filtered_event_dict['room_id'],
+        filtered_event_dict['room_name'],
+        filtered_event_dict['room_thread'],
+        filtered_event_dict['message'],
+        filtered_event_dict['timestamp']
+        ]
+    update_google_spreadsheet(values)
+    return json.jsonify({'text': event_dict})
     
 
 if __name__ == '__main__':
