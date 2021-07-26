c4ca7e3a3cf82c3667654edfe112d95d48bb7125
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-26 12:53:56 -0500

    Adding a flatten list function for thread unique function

--- common/helpers.py
+++ common/helpers.py
@@ -1,4 +1,10 @@
+from itertools import chain
+
+
 def create_values_dict(values):
     return {
         'values': values,
     }
+
+def flatten_list(_list):
+    return list(chain.from_iterable(_list))
