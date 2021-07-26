771529e6eddc4f3ce64b957cace81116208128eb
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-25 22:33:40 -0500

    Created private class methods to abstract code away

--- quickstart.py
+++ quickstart.py
@@ -11,43 +11,44 @@ import helpers as h
 
 class GoogleCredentials:
     def __init__(self, token_json, credentials_json, scopes):
-        self.token_json = token_json
         self.credentials_json = credentials_json
         self.scopes = scopes
-        self.creds = None
+        self.token_json = token_json
+        self.credentials = None
+                    
+    def get_credentials(self):
+        token_exists = self.__token_exists()
+        if token_exists:
+            self.credentials = Credentials.from_authorized_user_file(self.token_json, self.scopes)
+        token_invalid = self.__token_invalid()
+        if token_invalid:
+            token_expired = self.__token_expired()
+            if token_expired:
+                self.credentials.refresh(Request())
+            else:
+                self.credentials = self.__get_credentials_from_redirect()
+                print(self.credentials)
+            self.__create_token_json_file()
+        return self.credentials
     
-    def get_creds_from_redirect(self):
+    def __create_token_json_file(self):
+        with open(self.token_json, 'w') as token:
+            token.write(self.credentials.to_json())
+    
+    def __get_credentials_from_redirect(self):
         flow = InstalledAppFlow.from_client_secrets_file(self.credentials_json, self.scopes)
         return flow.run_local_server(port=0)
     
-    def create_token_json_file(self):
-        with open(self.token_json, 'w') as token:
-            token.write(self.creds.to_json())
-            
-    def get_auth_creds(self):
-        token_exists = self.token_exists()
-        if token_exists:
-            self.creds = Credentials.from_authorized_user_file(self.token_json, self.scopes)
-        token_invalid = self.token_invalid()
-        if token_invalid:
-            token_expired = self.token_expired()
-            if token_expired:
-                self.creds.refresh(Request())
-            else:
-                self.creds = self.get_creds_from_redirect()
-            self.create_token_json_file()
-        return self.creds
-    
-    def token_exists(self):
+    def __token_exists(self):
         if os.path.exists(self.token_json):
             return True
     
-    def token_expired(self):
-        if self.creds and self.creds.expired and self.creds.refresh_token:
+    def __token_expired(self):
+        if self.credentials and self.credentials.expired and self.credentials.refresh_token:
             return True
         
-    def token_invalid(self):
-        if not self.creds or not self.creds.valid:
+    def __token_invalid(self):
+        if not self.credentials or not self.credentials.valid:
             return True
         
 def create_google_service(creds, google_product, product_version):
@@ -64,7 +65,7 @@ def update_google_sheet(service, body, spreadsheet_id, sheet_range, value_input_
 if __name__ == '__main__':
     scopes = [c.READ_WRITE_SCOPE]
     creds_object = GoogleCredentials(c.TOKEN_JSON_FILE, c.CREDENTIALS_JSON_FILE, scopes)
-    creds = creds_object.get_auth_creds()
+    creds = creds_object.get_credentials()
     service = create_google_service(creds, c.GOOGLE_PRODUCT, c.PRODUCT_VERSION)
     values = [
         [
