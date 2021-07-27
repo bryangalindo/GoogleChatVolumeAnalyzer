dd5b82523eb8860d14cc84861034c600ebb596f0
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-27 09:11:19 -0500

    Adding more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,7 +6,8 @@ BLANK_STRING = ''
 EMPTY_NESTED_LIST = [[],[]]
 EMPTY_LIST = []
 THREAD_ID = 'Test'
-THREADS = [["Test 2"], ["Test 2"]]
+THREADS = [["Test"], ["Test"]]
+ALT_THREADS = [["Test2"], ["Test2"]]
 
 class TestIsFirstResponderFunction:
     def test_null_thread_list(self):
@@ -26,6 +27,13 @@ class TestIsFirstResponderFunction:
         
     def test_empty_thread_list(self):
         assert u.is_first_responder(THREAD_ID, EMPTY_NESTED_LIST) == None
+        
+    def test_duplicate_thread_id(self):
+        assert u.is_first_responder(THREAD_ID, THREADS) == False
+        
+    def test_valid_unique_thread_id(self):
+        assert u.is_first_responder(THREAD_ID, ALT_THREADS) == True
+        
         
         
     
