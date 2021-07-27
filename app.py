e8087ff916d830ea7ea5931d3297202768b5e344
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-27 16:06:53 -0500

    Adding if/else blocks to account for different event types

--- app.py
+++ app.py
@@ -25,19 +25,25 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def on_event():
     """Handles an event from Google Chat."""
-    event_dict = request.get_json()
-    filtered_event_dict = u.create_filtered_dict(event_dict)
-    values = [
-        filtered_event_dict['email'],
-        filtered_event_dict['room_id'],
-        filtered_event_dict['room_name'],
-        filtered_event_dict['thread_id'],
-        filtered_event_dict['message'],
-        filtered_event_dict['is_first_responder'],
-        filtered_event_dict['timestamp'],
-        ]
-    u.update_google_spreadsheet(values)
-    return json.jsonify({'text': str(event_dict)})
+    event = request.get_json()
+    if event['type'] == 'ADDED_TO_SPACE' and not event['space']['singleUserBotDm']:
+        text = 'Thanks for adding me to "%s"!' % (event['space']['displayName'] if event['space']['displayName'] else 'this chat')
+    elif event['type'] == 'MESSAGE':
+        filtered_event_dict = u.create_filtered_dict(event)
+        values = [
+            filtered_event_dict['email'],
+            filtered_event_dict['room_id'],
+            filtered_event_dict['room_name'],
+            filtered_event_dict['thread_id'],
+            filtered_event_dict['message'],
+            filtered_event_dict['is_first_responder'],
+            filtered_event_dict['timestamp'],
+            ]
+        u.update_google_spreadsheet(values)
+        text = 'Your message has been recorded as "%s"' % ('first responder' if filtered_event_dict['is_first_responder'] else 'participator')
+    else:
+        return 
+    return json.jsonify({'text': text})
     
 if __name__ == '__main__':
     print("Hello")
