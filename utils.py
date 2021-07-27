6068e10a8733eb2076b153a9773702695fe24a6c
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-27 18:13:38 -0500

    Troubleshooting

--- utils.py
+++ utils.py
@@ -12,7 +12,9 @@ threads = service.read_single_range(constants.SPREADSHEET_ID, constants.THREAD_I
 def is_first_responder(thread_id: str, threads: list) -> bool:
     if threads and thread_id:
         thread_id_list = helpers.flatten_list(threads)
+        print(thread_id_list)
         if thread_id_list:
+            print(thread_id)
             return helpers.string_is_unique(thread_id, thread_id_list)
 
 def create_filtered_dict(_dict: dict) -> dict:
