30aaf491072f5868e9731923ba3a26660261b4e2
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-27 22:44:17 -0500

    Adding 'service' positional argument to resolve unhandled TypeError

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def on_event():
             responder_flag,
             filtered_event_dict['timestamp'],
             ]
-        u.update_google_spreadsheet(values)
+        u.update_google_spreadsheet(values, service)
         text = "Got you down as a {}!".format('first responder' if filtered_event_dict['is_first_responder'] == True else 'participator')
     else:
         return "It's been real"
