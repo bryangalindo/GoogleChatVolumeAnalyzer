4dbdb3fd70c5d43c482e937578414d4684c6dddb
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-27 19:26:14 -0500

    Refactored create_filtered_dict function by extracting out function to keep SRP

--- utils.py
+++ utils.py
@@ -1,42 +1,33 @@
-from common import (constants, helpers)
-from models.GoogleCredentials import GoogleCredentials
-from models.GoogleService import GoogleService
+from common import constants as c
+from common import helpers as h
 
-
-scopes = [constants.READ_WRITE_SCOPE]
-creds = GoogleCredentials(constants.TOKEN_JSON_FILE, constants.CREDENTIALS_JSON_FILE, scopes)
-creds = creds.get_oauth_credentials()
-service = GoogleService(creds, constants.GOOGLE_PRODUCT, constants.PRODUCT_VERSION)
-GLOBAL_THREADS = service.read_single_range(constants.SPREADSHEET_ID, constants.THREAD_ID_SHEET_RANGE)
 
 def is_first_responder(thread_id: str, threads: list) -> bool:
     if threads and thread_id:
-        thread_id_list = helpers.flatten_list(threads)
+        thread_id_list = h.flatten_list(threads)
         if thread_id_list:
-            return helpers.string_is_unique(thread_id, thread_id_list)
+            return h.string_is_unique(thread_id, thread_id_list)
 
 def create_filtered_dict(_dict: dict) -> dict:
-    room_path_list = _dict['message']['thread']['name'].split('/')
-    thread_id = room_path_list[3]
-    room_id = room_path_list[1]
-    responder_flag = is_first_responder(thread_id, GLOBAL_THREADS)
-    return {
-        'timestamp': _dict['eventTime'],
-        'email': _dict['message']['sender']['email'],
-        'room_id': room_id,
-        'thread_id': thread_id,
-        'room_name': _dict['message']['space']['displayName'],
-        'message': _dict['message']['argumentText'],
-        'is_first_responder': responder_flag
-    }
+    root_message_dict = _dict.get('message')
+    if root_message_dict:
+        room_parameters = root_message_dict['thread']['name'].split('/')
+        thread_id = room_parameters[3]
+        room_id = room_parameters[1]
+        return {
+            'timestamp': _dict['eventTime'],
+            'email': root_message_dict['sender']['email'],
+            'room_id': room_id,
+            'thread_id': thread_id,
+            'room_name': root_message_dict['space']['displayName'],
+            'message': root_message_dict['argumentText'],
+        }
 
-def update_google_spreadsheet(record):
-    body = helpers.create_values_dict([record])
+def update_google_spreadsheet(record, service):
+    body = h.create_values_dict([record])
     service.insert_row_into_spreadsheet(
         body, 
-        constants.SPREADSHEET_ID, 
-        constants.SHEET_RANGE, 
-        constants.VALUE_INPUT_OPTION
+        c.SPREADSHEET_ID, 
+        c.SHEET_RANGE, 
+        c.VALUE_INPUT_OPTION
         )
-    global GLOBAL_THREADS
-    GLOBAL_THREADS = service.read_single_range(constants.SPREADSHEET_ID, constants.THREAD_ID_SHEET_RANGE)
