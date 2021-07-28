b098b2dfae99a3fa5ccfe849ec1fa0e4ea4caeea
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-28 13:34:01 -0500

    Adding tests for create filtered dict function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,5 @@
 import utils as u
 
-
 _NULL = None
 BLANK_STRING = ''
 EMPTY_NESTED_LIST = [[],[]]
@@ -8,6 +7,41 @@ EMPTY_LIST = []
 THREAD_ID = 'Test'
 THREADS = [["Test"], ["Test"]]
 ALT_THREADS = [["Test2"], ["Test2"]]
+VALID_EVENT_RESPONSE_DICT = {
+    'message': {'argumentText': 'Test',
+    'createTime': '2021-07-27T05:50:55.079839Z',
+    'sender': {'avatarUrl': 'url', 'displayName': 'Test User', 'domainId': 'domainId',
+               'email': 'test@test.com', 'name': 'users/123456', 'type': 'HUMAN'},
+    'space': {'displayName': 'Room Name', 'name': 'spaces/A', 
+              'threaded': True,'type': 'ROOM'},
+    'thread': {'name': 'spaces/A/threads/B', 'retentionSettings': {'state': 'PERMANENT'}}
+    },
+    }
+EXPECTED_FILTERED_DICT = {
+    'timestamp': '2021-07-27T05:50:55.079839Z', 'email': 'test@test.com',
+    'room_id': 'A', 'thread_id': 'B', 'room_name': 'Room Name',
+    'message': 'Test', 'user_id': 'users/123456',
+    }
+MISSING_MESSAGE_KEY_RESPONSE_DICT = {
+    'missing': {'argumentText': 'Test',
+    'createTime': '2021-07-27T05:50:55.079839Z',
+    'sender': {'avatarUrl': 'url', 'displayName': 'Test User', 'domainId': 'domainId',
+               'email': 'test@test.com', 'name': 'users/123456', 'type': 'HUMAN'},
+    'space': {'displayName': 'Room Name', 'name': 'spaces/A', 
+              'threaded': True,'type': 'ROOM'},
+    'thread': {'name': 'spaces/A/threads/B', 'retentionSettings': {'state': 'PERMANENT'}}
+    },
+    }
+MISSING_ARGUMENT_TEXT_KEY_RESPONSE_DICT = {
+    'message': {'missing': 'Test',
+    'createTime': '2021-07-27T05:50:55.079839Z',
+    'sender': {'avatarUrl': 'url', 'displayName': 'Test User', 'domainId': 'domainId',
+               'email': 'test@test.com', 'name': 'users/123456', 'type': 'HUMAN'},
+    'space': {'displayName': 'Room Name', 'name': 'spaces/A', 
+              'threaded': True,'type': 'ROOM'},
+    'thread': {'name': 'spaces/A/threads/B', 'retentionSettings': {'state': 'PERMANENT'}}
+    },
+    }
 
 class TestIsFirstResponderFunction:
     def test_null_thread_list(self):
@@ -33,6 +67,17 @@ class TestIsFirstResponderFunction:
         
     def test_valid_unique_thread_id(self):
         assert u.is_first_responder(THREAD_ID, ALT_THREADS) == True
+        
+class TestCreateFilteredDict:
+    def test_valid_event_dict(self):
+        assert u.create_filtered_dict(VALID_EVENT_RESPONSE_DICT) == EXPECTED_FILTERED_DICT
+        
+    def test_missing_message_key(self):
+        assert u.create_filtered_dict(MISSING_MESSAGE_KEY_RESPONSE_DICT) is None
+        
+    def test_missing_argument_text_key(self):
+        EXPECTED_FILTERED_DICT['message'] = None
+        assert u.create_filtered_dict(MISSING_ARGUMENT_TEXT_KEY_RESPONSE_DICT) == EXPECTED_FILTERED_DICT
         
         
         
