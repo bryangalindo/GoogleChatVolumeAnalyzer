4eaa2dd2a28159a510530c85e8196c7e678a106e
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-27 19:27:53 -0500

    Including threads getter inside the app route to refresh threads list after every message

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 from dotenv import load_dotenv
 
+from common import constants as c
+from common import helpers as h
+from models.GoogleCredentials import GoogleCredentials
+from models.GoogleService import GoogleService
 import utils as u
 
 load_dotenv()
@@ -20,6 +24,10 @@ sentry_sdk.init(
     traces_sample_rate=1.0
 )
 
+scopes = [c.READ_WRITE_SCOPE]
+creds = GoogleCredentials(c.TOKEN_JSON_FILE, c.CREDENTIALS_JSON_FILE, scopes).get_oauth_credentials()
+service = GoogleService(creds, c.GOOGLE_PRODUCT, c.PRODUCT_VERSION)
+
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
@@ -29,14 +37,16 @@ def on_event():
     if event['type'] == 'ADDED_TO_SPACE':
         text = 'Thanks for adding me to *%s*!' % (event['space']['displayName'] if event['space']['displayName'] else 'this chat')
     elif event['type'] == 'MESSAGE':
+        threads = service.read_single_range(c.SPREADSHEET_ID, c.THREAD_ID_SHEET_RANGE)
         filtered_event_dict = u.create_filtered_dict(event)
+        responder_flag = h.is_first_responder(filtered_event_dict['thread_id'], threads)
         values = [
             filtered_event_dict['email'],
             filtered_event_dict['room_id'],
             filtered_event_dict['room_name'],
             filtered_event_dict['thread_id'],
             filtered_event_dict['message'],
-            filtered_event_dict['is_first_responder'],
+            responder_flag,
             filtered_event_dict['timestamp'],
             ]
         u.update_google_spreadsheet(values)
