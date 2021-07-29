109e56faf1ed7afadb118da5b9a8e6368973ac2b
Bryan Galindo <galindo.bryan08@gmail.com>
2021-07-29 10:05:17 -0500

    Making sheet range an environment variable to avoid cahnging the source code

--- common/constants.py
+++ common/constants.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
-SHEET_RANGE = 'Sheet2'
+SHEET_RANGE = os.getenv('SHEET_RANGE')
 READ_WRITE_SCOPE = 'https://www.googleapis.com/auth/spreadsheets'
 TOKEN_JSON_FILE = 'token.json'
 CREDENTIALS_JSON_FILE = 'credentials.json'
